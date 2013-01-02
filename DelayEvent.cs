a08c8824a8aa84134f93cc03d6e3352d7a3887f4
heavenwing <redmoon17@gmail.com>
2013-01-02 22:45:01 +0800

    DelayEvent

---
+++ DelayEvent.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace ITKE.WinLib
+{
+    public partial class DelayEvent : Component
+    {
+        public DelayEvent()
+        {
+            InitializeComponent();
+        }
+
+        public DelayEvent(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        [DefaultValue(500)]
+        public int Interval
+        {
+            get { return timer1.Interval; }
+            set { timer1.Interval = value; }
+        }
+
+        public event Action ActualDo;
+
+        public object[] Parameters;
+        public void Do(params object[] parameters)
+        {
+            timer1.Stop();
+            Parameters = parameters;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (ActualDo != null)
+                ActualDo();
+        }
+    }
+}
